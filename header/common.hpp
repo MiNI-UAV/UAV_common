d725f179a601cee6232ee206bdef831a5c82505a
Wojciech Gajda <wggajda@gmail.com>
2023-10-19 22:25:08 +0200

    delete: old RK4 class

--- header/common.hpp
+++ header/common.hpp
@@ -2,11 +2,9 @@
 
 #include "../src/logger/logger.hpp"
 #include "../src/ode/ode.hpp"
-#include "../src/RK4/RK4.hpp"
 #include "../src/PID/PID.hpp"
 #include "../src/timed_loop/timed_loop.hpp"
 #include "../src/timed_loop/status.hpp"
-#include "../src/RK4/RK4.hpp"
 #include "../src/parser/parser.hpp"
 #include "../src/parser/uav_params.hpp"
 #include "../src/components/components.hpp"
