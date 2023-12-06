08f57f27ff773405ab084fa5ac920f276eb8b6c9
Wojciech Gajda <wggajda@gmail.com>
2023-12-06 16:16:49 +0100

    added: bang bang controller

--- src/controllers/impl/bang_bang.hpp
+++ src/controllers/impl/bang_bang.hpp
@@ -9,16 +9,22 @@ namespace controllers
     class BangBang : public Controller
     {
         public:
-        
+
+            /// @brief Constructor with all Bang-bang controller parameters
+            /// @param high output when error is positive
+            /// @param low output when error is negative
+            /// @param delta histeresis symetrical to zero
+            BangBang(double high, double low, double delta = 0.0);
+
             /// @brief Construct controller with parameters from xml
             /// @param controller_node xml node with controller params
             BangBang(rapidxml::xml_node<>* controller_node);
 
             /// @brief calc output of controller with specific time step
             /// @param error input of controller
-            /// @param dt time step
+            /// @param dt time step, unused
             /// @return output of controller
-            double calc(double error, double dt) override;
+            double calc(double error, [[maybe_unused]] double dt) override;
 
             /// @brief clear internal state
             void clear() override;
@@ -27,6 +33,10 @@ namespace controllers
             std::unique_ptr<Controller> clone() const override;
 
         private:
+            double _high;
+            double _low;
+            double _delta;
+            int _last_output_sign;
 
     };
 }
