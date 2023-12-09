98a1c167cd8670bfec40fab4c566f70568d89835
Wojciech Gajda <wggajda@gmail.com>
2023-12-09 14:15:40 +0100

    bugfix: common.hpp

--- header/common.hpp
+++ header/common.hpp
@@ -2,7 +2,7 @@
 
 #include "../src/logger/logger.hpp"
 #include "../src/ode/ode.hpp"
-#include "../src/PID/PID.hpp"
+#include "../src/controllers/controller.hpp"
 #include "../src/timed_loop/timed_loop.hpp"
 #include "../src/timed_loop/status.hpp"
 #include "../src/parser/parser.hpp"
